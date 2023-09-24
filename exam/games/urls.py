31fc1315eb0f8a93e268d7d442f574a37e10940a
MMM <ediconpts@gmail.com>
2023-09-24 12:48:50 +0500

    qwerty

--- exam/games/urls.py
+++ exam/games/urls.py
@@ -4,6 +4,6 @@ from .views import *
 
 urlpatterns = [
     path("", first_view, name="first_view"),
-    path("details/<int:user_id>", game_details, name="game_details"),
+    path("details/<int:game_id>", game_details, name="game_details"),
     path("delete/<int:del_id>", delete_game, name="delete_game")
 ]
